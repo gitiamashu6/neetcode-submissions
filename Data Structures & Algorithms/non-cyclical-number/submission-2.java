9dce17e03dd57cb20eb691c796d48007312b8898
ASHUTOSH RANJAN <iamashu6@gmail.com>
2026-09-21 01:13:29 +0530

    Add: non-cyclical-number - submission-2

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        return digitSum(n, set);
+    }
+
+    private boolean digitSum(int n, Set set) {
+        int sum = 0;
+        while(n != 0) {
+            int r = n % 10;
+            n = n / 10;
+            sum += r*r;
+        }
+        if(sum == 1) return true;
+        else if(!set.add(sum)) return false;
+        else {
+            return digitSum(sum, set);
+        }
+    }
+}
